4a4281fbdcf1f71752d79e67fd3170a8f4d3ba2d
Christopher Santana <christopher.santana@nav.no>
2021-01-26 13:31:17 +0100

    Forsøk å bumpe runner OS.

--- push_workflow_files.sh
+++ push_workflow_files.sh
@@ -11,7 +11,7 @@ function createNode {
   CONTENT=$(cat $1)
   echo $(jq -n -c \
               --arg path ".github/workflows/$WORKFLOW_FILE_NAME" \
-              --rawfile $1 \
+              --rawfile content $1 \
               '{ path: $path, mode: "100644", type: "blob", content: $content }'
   )
 }
