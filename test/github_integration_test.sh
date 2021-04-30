c662a9802024dcc7d9508153a918a7748f2ba261
Christopher Santana <christopher.santana@nav.no>
2021-04-30 10:00:18 +0200

    Endrer '.yml' extension til '.yaml'.

--- test/github_integration_test.sh
+++ test/github_integration_test.sh
@@ -3,7 +3,7 @@
 ## Use current time to create new and unique workflow file.
 DATETIME=$(date +%Y%m%d%H%M%S)
 
-sed -i "s/Datetime - [0-9]\{14\}/Datetime - $DATETIME/" './test/workflows/__DISTRIBUTED_dummy.yml'
+sed -i "s/Datetime - [0-9]\{14\}/Datetime - $DATETIME/" './test/workflows/__DISTRIBUTED_dummy.yaml'
 
 
 ## Use script to apply workflow to remote repository
@@ -14,19 +14,19 @@ sed -i "s/Datetime - [0-9]\{14\}/Datetime - $DATETIME/" './test/workflows/__DIST
 CURRENT_MAIN_SHA=$(curl -s -u "$API_ACCESS_TOKEN:" "https://api.github.com/repos/navikt/pb-workflow-authority-test-dummy/git/refs/heads/main" | jq -r '.object.sha')
 
 ## Find sha of remote workflow file after changes were attempted.
-DUMMY_WORKFLOW_SHA=$(curl -s -u "$API_ACCESS_TOKEN:" "https://api.github.com/repos/navikt/pb-workflow-authority-test-dummy/git/trees/$CURRENT_MAIN_SHA?recursive=1" | jq -r '.tree[] | select(.path == ".github/workflows/dummy.yml").sha')
+DUMMY_WORKFLOW_SHA=$(curl -s -u "$API_ACCESS_TOKEN:" "https://api.github.com/repos/navikt/pb-workflow-authority-test-dummy/git/trees/$CURRENT_MAIN_SHA?recursive=1" | jq -r '.tree[] | select(.path == ".github/workflows/dummy.yaml").sha')
 
 ## Exit with error if file was not found
 if [[ -z $DUMMY_WORKFLOW_SHA ]]; then
-  echo "dummy.yml workflow file was not found in destionation repository."
+  echo "dummy.yaml workflow file was not found in destionation repository."
   exit 1
 fi
 
 ## Find contents of remote file
-curl -s -u "$API_ACCESS_TOKEN:" "https://api.github.com/repos/navikt/pb-workflow-authority-test-dummy/git/blobs/$DUMMY_WORKFLOW_SHA" | jq -r '.content' | base64 -d >> ./test/workflows/dummy.yml
+curl -s -u "$API_ACCESS_TOKEN:" "https://api.github.com/repos/navikt/pb-workflow-authority-test-dummy/git/blobs/$DUMMY_WORKFLOW_SHA" | jq -r '.content' | base64 -d >> ./test/workflows/dummy.yaml
 
 ## Verify that contents of remote file matches local file
-if ! diff -q './test/workflows/dummy.yml' './test/workflows/__DISTRIBUTED_dummy.yml' &>/dev/null; then
+if ! diff -q './test/workflows/dummy.yaml' './test/workflows/__DISTRIBUTED_dummy.yaml' &>/dev/null; then
   echo 'Failed in applying changes to remote repository.'
   exit 1
 fi
